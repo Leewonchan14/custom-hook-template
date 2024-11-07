f2957351adf35cd5e77dc38c1ab7593467e75a72
이원찬 <twoone14@tukorea.ac.kr>
2024-11-07 17:47:32 +0900

    Feat : useThrottle 커스텀 훅 구현

---
+++ src/hooks/useThrottle.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from 'react';
+
+export default function useThrottle<T>(value: T, limit: number): T {
+  const [throttledValue, setThrottledValue] = useState(value);
+  const lastRan = useRef(Date.now());
+
+  useEffect(() => {
+    const handle = setTimeout(
+      () => {
+        setThrottledValue(value);
+        lastRan.current = Date.now();
+      },
+      limit - (Date.now() - lastRan.current),
+    );
+
+    return () => {
+      clearTimeout(handle);
+    };
+  }, [limit, value]);
+
+  return throttledValue;
+}
