f462d9d93e1158da20bc2dcf12af4c35cc84e08c
이원찬 <twoone14@tukorea.ac.kr>
2024-11-07 17:18:15 +0900

    Chore : eslint prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -25,4 +29,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
