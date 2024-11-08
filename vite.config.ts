604ce008f2bf568c170bf86f650e47f60a1b109a
이원찬 <twoone14@tukorea.ac.kr>
2024-11-08 11:54:50 +0900

    Chore : vite.config.ts prettier fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import checker from "vite-plugin-checker";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +10,7 @@ export default defineConfig({
     tsconfigPaths(),
     checker({
       typescript: {
-        tsconfigPath: "tsconfig.app.json",
+        tsconfigPath: 'tsconfig.app.json',
       },
     }),
   ],
