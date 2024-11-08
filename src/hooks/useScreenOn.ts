9c77c0c21cafd6aae4b67846d44b78dbcd695187
이원찬 <twoone14@tukorea.ac.kr>
2024-11-08 13:48:01 +0900

    ✨ Feat : useScreenOn 다양한 상황에 맞게 재사용 가능하게 리팩토링

--- src/hooks/useScreenOn.ts
+++ src/hooks/useScreenOn.ts
@@ -1,23 +1,47 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
-const useScreenOn = (marginOffset = 0) => {
+const useScreenOn = (margin?: {
+  top?: string;
+  right?: string;
+  bottom?: string;
+  left?: string;
+}) => {
+  const { top, right, bottom, left } = margin ?? {
+    top: '0px',
+    right: '0px',
+    bottom: '0px',
+    left: '0px',
+  };
   const targetRef = useRef(null);
+  const [isCanObserve, setIsCanObserve] = useState(true);
   const [isOnScreen, setIsOnScreen] = useState(false);
 
   useEffect(() => {
-    if (!targetRef.current) return;
+    if (!targetRef.current || !isCanObserve) return;
 
     const observer = new IntersectionObserver(
       ([entry]) => setIsOnScreen(entry.isIntersecting),
-      { rootMargin: `${marginOffset}px` },
+      {
+        rootMargin: `${top ?? '0px'} ${right ?? '0px'} ${bottom ?? '0px'} ${
+          left ?? '0px'
+        }`,
+      },
     );
 
     observer.observe(targetRef.current);
 
     return () => observer.disconnect();
-  }, [marginOffset]);
+  }, [top, right, bottom, left, isCanObserve]);
 
-  return { isOnScreen, targetRef };
+  const disconnect = useCallback(() => {
+    setIsCanObserve(false);
+  }, []);
+
+  const connect = useCallback(() => {
+    setIsCanObserve(true);
+  }, []);
+
+  return { isOnScreen, targetRef, disconnect, connect };
 };
 
 export default useScreenOn;
