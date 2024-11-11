14190f0ad5578e0dbba7fdf1f9877d45c078a031
이원찬 <twoone14@tukorea.ac.kr>
2024-11-11 14:07:29 +0900

    useAddScrollEvent, useSmoothApproach 사용 예시 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useCallback, useState } from 'react';
 import './App.css';
+import useAddScrollEvent from './hooks/useAddScrollEvent';
+import useSmoothApproach from './hooks/useSmoothApproach';
 
-function App() {
-  const [count, setCount] = useState(0);
+const covertScrollToGoal = (
+  value: number,
+  range: { min: number; max: number },
+  limit: { min: number; max: number },
+) => {
+  return Math.max(
+    limit.min,
+    Math.min(
+      limit.max,
+      ((value - range.min) / (range.max - range.min)) *
+        (limit.max - limit.min),
+    ),
+  );
+};
+
+export default function App() {
+  const [scroll, setScroll] = useState(0);
+  const [goal, setGoal] = useState(0);
+
+  const scrollHandler = useCallback(() => {
+    setScroll(window.scrollY);
+    setGoal(
+      covertScrollToGoal(
+        window.scrollY,
+        { min: 100, max: 500 },
+        { min: 100, max: 1500 },
+      ),
+    );
+  }, []);
+
+  useAddScrollEvent({ callback: scrollHandler, isWindow: true });
+
+  const { smooth: smoothPosition } = useSmoothApproach({
+    goal,
+    minTick: 0.05,
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-[200vh] flex items-center">
+      <div className="fixed font-bold text-3xl bottom-10 whitespace-pre-wrap">
+        smoothPosition: {smoothPosition}
+        {'\n'}
+        scroll : {scroll}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div
+        className={`fixed top-0 w-48 p-4 bg-blue-500 text-white text-center rounded-lg transition-transform duration-200 ease-linear`}
+        style={{ transform: `translateX(${smoothPosition}px)` }}
+      >
+        {smoothPosition} 스크롤 정도에 따라 이동하는 요소{' '}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="absolute invisible h-screen"></div>
+    </div>
   );
 }
-
-export default App;
